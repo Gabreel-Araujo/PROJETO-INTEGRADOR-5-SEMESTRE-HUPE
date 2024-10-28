4a0af3477a233f6a64f3cb63a4c3cdc8883e3d02
Gabriel Alves <87396370+Gabreel-Araujo@users.noreply.github.com>
2024-10-28 17:29:35 -0300

    feat: create config to prisma and use docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
 import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.customer.create({
+	data: {
+		name: 'Gabriel alves',
+		email: 'gabriel@gabriel.com',
+		password: '123456',
+	},
+});
