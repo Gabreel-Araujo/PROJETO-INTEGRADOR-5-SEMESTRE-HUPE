67759feaaec9a5229318b7aa4a555688ca7fabea
Gabriel Alves <87396370+Gabreel-Araujo@users.noreply.github.com>
2024-11-18 17:36:03 -0300

    feat: Initial config entitie  Oder

---
+++ src/modules/order/controllers/OrderController.ts
@@ -0,0 +1,26 @@
+import { FastifyRequest, FastifyReply } from 'fastify';
+import { OrderService } from '../services/OrderService';
+import { CreateOrderBody } from '../interfaces/OrderInterface';
+
+export class OrderController {
+	private orderService = new OrderService();
+
+	// Usando o tipo CreateOrderBody para garantir que o corpo da requisição tenha a estrutura correta
+	async createOrder(
+		request: FastifyRequest<{ Body: CreateOrderBody }>,
+		reply: FastifyReply,
+	): Promise<void> {
+		const { customerId, entrepreneurId, items } = request.body;
+
+		try {
+			const order = await this.orderService.createOrder(
+				customerId,
+				entrepreneurId,
+				items,
+			);
+			reply.status(201).send(order);
+		} catch (error) {
+			reply.status(400).send({ error });
+		}
+	}
+}
